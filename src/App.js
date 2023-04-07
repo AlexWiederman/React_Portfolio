6a35fbd2f2ccb805762dd12be3611403bf0b960b
Alex W <amwiederman15@gmail.com>
2023-04-07 08:38:18 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-const App = () => <PortfolioContainer />;
+function App() {
+    return(
+        <><div>
+            <Header />
+        </div><div>
+                <PortfolioContainer />
+            </div>
+            <div>
+                <Footer /></div></>
+
+    )
+}
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div class="p-5 text-center bg-light" >
+    <h1 class="mb-3">Alex Wiederman</h1>
+    {/* <h4 class="mb-3">Portfolio</h4> */}
+  </div>
+  )
 }
 
