e9bba5ba3501264d61b612acbe75144333e3d32d
Alex W <amwiederman15@gmail.com>
2023-04-08 20:05:13 -0400

    added portfolio first image

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
+import "./Style.css";
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+
+      {/*  Project Pictures */}
+      <div class="container">
+        <div class="card" style={{ width: "18rem" }}>
+          <img
+            class="card-img-top"
+            src={process.env.PUBLIC_URL + "/project2.png"}
+            alt="No Crude Food Project"
+          ></img>
+          <div class="card-body">
+            <h5 class="card-title">No Crude Food Project</h5>
+            <a
+              href="https://github.com/AlexWiederman/No_Crude_Food"
+              class="btn btn-primary"
+            >
+              Github Link
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
